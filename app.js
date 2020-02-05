c5624fd29add79f935049020980ae1e149e4e2d1
Alyssa Cucunato <acucunato18@gmail.com>
2020-02-05 18:02:01 -0500

    added prompt functionality, need to loop through res to create cards for html file

--- app.js
+++ app.js
@@ -5,12 +5,121 @@ const Manager = require("./lib/manager");
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-function promptUser() {
-  return inquirer.prompt([
+const createTeam = [];
+
+//create fx for specific questions general prompts
+const questions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is your employee's name?"
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your employee's email?"
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is your employee's ID?"
+  }
+];
+
+//intern specific
+const internPrompt = [
+  {
+    type: "input",
+    name: "school",
+    message: "What is your employee's school?"
+  }
+];
+
+//eng specific
+const engineerPrompt = [
+  {
+    type: "input",
+    name: "github",
+    message: "What is your employee's GitHub username?"
+  }
+];
+
+inquirer
+  .prompt([
     {
       type: "input",
       name: "name",
-      message: "What is your name?"
+      message: "What is your manager's name?"
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your manager's email?"
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is your manager's ID?"
+    },
+    {
+      type: "input",
+      name: "officeNumber",
+      message: "What is your manager's office number?"
     }
-  ]);
+  ])
+  .then(res => {
+    createTeam.push(new Manager(res.name, res.email, res.id, res.officeNumber));
+
+    addRoles();
+  });
+
+//add new employees
+function addRoles() {
+  return (
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          message: "Which type of team member would you like to add?",
+          choices: ["Engineer", "Intern", "None"],
+          name: "employee"
+        }
+      ])
+      // new array to handle questions/answers
+      .then(res => {
+        let team = [];
+        switch (res.employee) {
+          case "Engineer":
+            team = engineerPrompt;
+            break;
+          case "Intern":
+            team = internPrompt;
+            break;
+          case "None":
+            let HTML = generateHTML();
+            fs.writeFile("./output/team.html", html, err => {
+              if (err) {
+                console.log(err);
+              }
+            });
+        }
+        // add both arrays together push to create team array
+        inquirer.prompt(questions.concat(team)).then(ans => {
+          switch (res.employee) {
+            case "Engineer":
+              createTeam.push(
+                new Engineer(ans.name, ans.id, ans.email, ans.github)
+              );
+              break;
+            case "Intern":
+              createTeam.push(
+                new Intern(ans.name, ans.id, ans.email, ans.school)
+              );
+              break;
+          }
+          console.log(createTeam);
+          addRoles();
+        });
+      })
+  );
 }
